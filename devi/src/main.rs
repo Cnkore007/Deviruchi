522e6e279558959fa1bf078d88bc386924c4d1d6
cnkore007 <nikcoz1984@Gmail.com>
2026-05-04 01:26:24 +0800

    feat(devi): add WebSocket network client

--- devi/src/main.rs
+++ devi/src/main.rs
@@ -5,11 +5,22 @@ use devi::game::map::GameMap;
 use devi::render::tile::{setup_map, setup_local_player};
 use devi::render::camera::follow_camera;
 use devi::game::input::handle_input;
+use devi::network::{NetworkClient, Packet};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(Resource)]
+struct NetworkResource {
+    client: Arc<Mutex<Option<NetworkClient>>>,
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
         .insert_resource(GameMap::new(30, 20, 32.0))
+        .insert_resource(NetworkResource {
+            client: Arc::new(Mutex::new(None)),
+        })
+        .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup_map, setup_local_player))
         .add_systems(Update, (handle_input, follow_camera))
         .run();
---
+++ devi/src/network/mod.rs
@@ -0,0 +1,5 @@
+pub mod client;
+pub mod protocol;
+
+pub use client::NetworkClient;
+pub use protocol::Packet;
